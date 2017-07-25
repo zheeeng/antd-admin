67e442f2479380ccc1eb7dfa550ed521bf9ba767
Zheeeng <hi@zheeeng.me>
2017-07-25 12:49:32 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ import createLoading from 'dva-loading'
 import { browserHistory } from 'dva/router'
 import { message } from 'antd'
 
+const testFunc = () => {
+  const message = `rewrited message`
+  const arr = [{
+    message: `I'm string message, not component!`,
+    dva: `I'm string dva`,
+    createLoading: `I'm string createLoading`,
+    browserHistory: `I'm string browserHistory`
+  }]
+  const res = arr.map(({ message, dva, createLoading, browserHistory }) => ({ message, dva, createLoading, browserHistory }))
+  console.log(res)
+}
+
+testFunc()
+
 // 1. Initialize
 const app = dva({
   ...createLoading({
